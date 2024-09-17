548bac064076f578166c2eac38d9303aba722727
Colombo.20108 <Colombo.20108@LABI1-6PC23>
2024-09-17 10:24:42 +0200

    Aggiungere i file di progetto.

---
+++ SerializzaDeserializza/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SerializzaDeserializza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_titolo = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Titolo = new System.Windows.Forms.Label();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_autore = new System.Windows.Forms.TextBox();
+            this.listbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txt_titolo
+            // 
+            this.txt_titolo.Location = new System.Drawing.Point(372, 186);
+            this.txt_titolo.Name = "txt_titolo";
+            this.txt_titolo.Size = new System.Drawing.Size(100, 20);
+            this.txt_titolo.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Titolo
+            // 
+            this.Titolo.AutoSize = true;
+            this.Titolo.Location = new System.Drawing.Point(369, 170);
+            this.Titolo.Name = "Titolo";
+            this.Titolo.Size = new System.Drawing.Size(33, 13);
+            this.Titolo.TabIndex = 2;
+            this.Titolo.Text = "Titolo";
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(210, 165);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(117, 61);
+            this.btn_create.TabIndex = 3;
+            this.btn_create.Text = "CREATE";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(210, 251);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(117, 61);
+            this.btn_read.TabIndex = 4;
+            this.btn_read.Text = "READ";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(210, 328);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(117, 61);
+            this.btn_update.TabIndex = 5;
+            this.btn_update.Text = "UPDATE";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(210, 411);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(117, 61);
+            this.btn_delete.TabIndex = 6;
+            this.btn_delete.Text = "DELETE";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(369, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Autore";
+            // 
+            // txt_autore
+            // 
+            this.txt_autore.Location = new System.Drawing.Point(372, 251);
+            this.txt_autore.Name = "txt_autore";
+            this.txt_autore.Size = new System.Drawing.Size(100, 20);
+            this.txt_autore.TabIndex = 7;
+            // 
+            // listbox
+            // 
+            this.listbox.FormattingEnabled = true;
+            this.listbox.Location = new System.Drawing.Point(557, 170);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(234, 303);
+            this.listbox.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1484, 835);
+            this.Controls.Add(this.listbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_autore);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.Titolo);
+            this.Controls.Add(this.txt_titolo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_titolo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label Titolo;
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_autore;
+        private System.Windows.Forms.ListBox listbox;
+    }
+}
+
---
+++ SerializzaDeserializza/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerializzaDeserializza
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Libreria libreria = new Libreria();
+        class Libri
+        {
+            
+            public string _titolo { get ; set; }
+            public string _autore { get; set; }
+
+            public Libri()
+            { }
+            public Libri(string titolo, string autore)
+            {
+                _titolo = titolo;
+                _autore = autore;
+            }
+            public string Serializza()
+            {
+                return $"{_titolo};{_autore}";
+            }        
+        }
+
+        class Libreria
+        {
+            public List<Libri> _libri = new List<Libri>();
+
+            public void Leggi()
+            {                
+                using (StreamReader sr = new StreamReader(@"Libri.txt"))
+                {
+
+                    string[] array = new string[2];
+                    string linea = sr.ReadLine();
+                    while (linea != null)
+                    {                   
+                        if(linea != null)
+                        {                          
+                            array = linea.Split(';');
+                            Libri libro = new Libri(array[0], array[1]);
+                            _libri.Add(libro);
+                        }
+                        linea = sr.ReadLine();
+                    }
+                }
+            }
+
+            public void Scrivi()
+            {
+                using (StreamWriter sw = new StreamWriter("Libri.txt"))
+                {
+                    for (int i = 0; i < _libri.Count; i++)
+                    {
+                        sw.WriteLine(_libri[i].Serializza());
+                    }
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("Libri.txt") == true)
+            {
+                libreria.Leggi();
+                for (int i = 0; i < libreria._libri.Count; i++)
+                {
+                    listbox.Items.Add(libreria._libri[i].Serializza());
+                }
+            }      
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            if(txt_titolo.Text != "" && txt_autore.Text != "")
+            {
+                libreria._libri.Add(new Libri(txt_titolo.Text, txt_autore.Text));
+            }
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            listbox.Items.Clear();
+            for(int i = 0; i < libreria._libri.Count; i++)
+            {
+                listbox.Items.Add(libreria._libri[i].Serializza());
+            }
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            if (txt_titolo.Text != "" && txt_autore.Text != "")
+            {
+                int a = listbox.SelectedIndex;
+                libreria._libri[a]._titolo = txt_titolo.Text;
+                libreria._libri[a]._autore = txt_autore.Text;
+            }
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+            int a = listbox.SelectedIndex;
+            libreria._libri.RemoveAt(a);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            libreria.Scrivi();
+        }
+    }
+}
